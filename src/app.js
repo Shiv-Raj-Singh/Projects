f76af540f0c4d3aaf5fdb9e7bc83219f73a59783
Shiv-Raj-Singh <mt932747@gmail.com>
2022-12-13 19:10:08 +0530

    done

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ const app = express()
 app.use(express.json())
 const port =  3000 || process.env.PORT
 
+app.use(express.Router().all('/*' , (req , res)=>{
+    return res.status(400).send({status:false, msg:"invalid http request !"})
+}))
+
 app.use('/' , router)
 app.listen(port , ()=>{
     console.log(`App is Running on Port NUmber ${port}`)
